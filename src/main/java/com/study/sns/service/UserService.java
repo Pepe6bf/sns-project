4fd5e12ac0ef729963ca15a9303b0e730080d5cf
pepe6bf <pepe6bf@gmail.com>
2022-11-11 19:18:44 +0900

    test: 회원가입 테스트 코드 작성을 위한 main로직 작성
    회원가입 테스트 코드를 작성하기 위해 아래와 같은 로직을 main에 작성함
    * UserController
    * UserService
    * User
    * UserjoinRequest
    * SnsApplicationException

---
+++ src/main/java/com/study/sns/service/UserService.java
@@ -0,0 +1,13 @@
+package com.study.sns.service;
+
+import com.study.sns.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    // TODO : implement
+    public User join() {
+        return new User();
+    }
+}
