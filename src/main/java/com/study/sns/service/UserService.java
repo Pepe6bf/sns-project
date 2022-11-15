250abd8a70c7718078372beb491a4f7ede7f876a
pepe6bf <pepe6bf@gmail.com>
2022-11-15 21:13:45 +0900

    refac: #6 UserService 회원가입 로직에 CreateUserDto.Request 적용

--- src/main/java/com/study/sns/service/UserService.java
+++ src/main/java/com/study/sns/service/UserService.java
@@ -1,8 +1,8 @@
 package com.study.sns.service;
 
 import com.study.sns.exception.SnsApplicationException;
+import com.study.sns.model.dto.CreateUserDto;
 import com.study.sns.model.entity.User;
-import com.study.sns.model.UserJoinResponseDto;
 import com.study.sns.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,21 +13,20 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    // TODO : implement
-    public UserJoinResponseDto join(String email,
-                                    String password
+    public CreateUserDto.Response join(String email,
+                                       String password
     ) {
-        // 회원가입하려는 email로 회원가입이된 user가 있는지 검증
+        // 이미 가입된 email 인지 검증
         userRepository
                 .findByEmail(email)
                 .ifPresent(it -> {
                     throw new SnsApplicationException();
                 });
 
-        // 회원가입 진행 = user를 등록
-        userRepository.save(new User());
-
-        return new UserJoinResponseDto();
+        // 회원가입 진행 및 응답
+        return CreateUserDto.Response.fromEntity(
+                userRepository.save(User.of(email, password))
+        );
     }
 
     // TODO : implement
