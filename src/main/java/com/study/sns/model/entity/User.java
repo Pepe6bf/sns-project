a040c4ba0ae46c5f444ef7f5a11d525af87d2aea
pepe6bf <pepe6bf@gmail.com>
2022-11-15 20:16:51 +0900

    feat: #6 User 엔티티 클래스 audit적용 및 팩토리 메서드 of 추가
    * UserEntity클래스에 BaseTimeEntity를 적용해주었다.
    * User인스턴스를 생성해서 반환하는 of메서드를 추가하였다.

--- src/main/java/com/study/sns/model/entity/User.java
+++ src/main/java/com/study/sns/model/entity/User.java
@@ -1,37 +1,41 @@
 package com.study.sns.model.entity;
 
+import com.study.sns.global.config.audit.BaseTimeEntity;
 import com.study.sns.model.constant.UserRole;
 import lombok.*;
-import org.hibernate.annotations.SQLDelete;
 
 import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
 @Table(name = "\"user\"")   // postgre를 사용할 경우 \"해줘야 함
 @Entity
-public class User {
+public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "role")
+    @Column(nullable = false, name = "role")
     @Enumerated(EnumType.STRING)
     private UserRole role = UserRole.USER;
 
-    @Column(name = "email")
+    @Column(nullable = false, name = "email")
     @Getter
     private String email;
 
-    @Column(name = "password")
+    @Column(nullable = false, name = "password")
     @Getter
     private String password;
 
-//    private static User of() {
-//
-//    }
+    private User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 
+    public static User of(String email,
+                          String password
+    ) {
+        return new User(email, password);
+    }
 }
