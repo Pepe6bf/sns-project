fd08c5bc746e5043e39d9ab21acb44157b747fbd
pepe6bf <pepe6bf@gmail.com>
2022-11-26 16:01:45 +0900

    feat: #6 User관련 DTO 분리
    service로직과 controller의 DTO를 최대한 분리하기 위해 UserDto를 새롭게 생성하였다.

---
+++ src/main/java/com/study/sns/model/dto/UserJoinDto.java
@@ -0,0 +1,44 @@
+package com.study.sns.model.dto;
+
+import com.study.sns.model.constant.UserRole;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserJoinDto {
+    @Getter
+    @AllArgsConstructor
+    public static class Request {
+
+        private String email;
+
+        private String password;
+    }
+
+    @Getter
+    public static class Response {
+        private final Long userId;
+        private final String email;
+        private UserRole role;
+
+        private Response(
+                Long userId,
+                String email,
+                UserRole role
+        ) {
+            this.userId = userId;
+            this.email = email;
+            this.role = role;
+        }
+
+        public static Response of(UserDto userDto) {
+            return new Response(
+                    userDto.getId(),
+                    userDto.getEmail(),
+                    userDto.getRole()
+            );
+        }
+    }
+}
