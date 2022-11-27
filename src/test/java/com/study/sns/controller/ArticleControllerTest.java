9f01966562b60b415246a2edc3d5531b8fa52f50
pepe6bf <pepe6bf@gmail.com>
2022-11-27 22:13:29 +0900

    test: #8 Article Controller/Service 테스트 코드 생성 및 초안 작성

---
+++ src/test/java/com/study/sns/controller/ArticleControllerTest.java
@@ -0,0 +1,72 @@
+package com.study.sns.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.sns.dto.CreateArticleDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ArticleControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @WithMockUser
+    @DisplayName("게시글 작성 성공 테스트")
+    void 게시글작성_성공() throws Exception {
+        // Given
+        String title = "test title";
+        String content = "test content";
+
+        // When
+        // Then
+        mockMvc.perform(post("/api/v1/article")
+                        .contentType(APPLICATION_JSON)
+                        .content(
+                                objectMapper.writeValueAsBytes(
+                                        new CreateArticleDto.Request(title, content)
+                                )
+                        )
+                ).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithAnonymousUser
+    @DisplayName("게시글 작성 실패 테스트 - 로그인 하지 않은 사용자")
+    void 게시글작성_실패_로그인하지않은_사용자() throws Exception {
+        // Given
+        String title = "test title";
+        String content = "test content";
+
+        // When
+        // Then
+
+        // 로그인 하지 않은 경우
+        mockMvc.perform(post("/api/v1/article")
+                        .contentType(APPLICATION_JSON)
+                        .content(
+                                objectMapper.writeValueAsBytes(
+                                        new CreateArticleDto.Request(title, content)
+                                )
+                        )
+                ).andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+}
