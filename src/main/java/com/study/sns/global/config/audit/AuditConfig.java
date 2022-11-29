4b38cfa48798d54b339953602468c97195005d25
pepe6bf <pepe6bf@gmail.com>
2022-11-29 11:21:41 +0900

    feat: #9 Audit 리소스 생성자에 현재 로그인 중인 사용자의 정보가 들어가도록 개발

--- src/main/java/com/study/sns/global/config/audit/AuditConfig.java
+++ src/main/java/com/study/sns/global/config/audit/AuditConfig.java
@@ -5,14 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.Optional;
-
 @Configuration
 @EnableJpaAuditing
 public class AuditConfig {
-
     @Bean
     public AuditorAware<String> auditorProvider() {
-        return () -> Optional.of("tester");
+        return new AuditorAwareImpl();
     }
 }
