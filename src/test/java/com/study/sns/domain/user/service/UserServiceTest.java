f4ea5ba902d9c40487a4c36fa3f396d9654870ac
pepe6bf <pepe6bf@gmail.com>
2022-12-08 22:39:27 +0900

    test: #12 UserServiceTest 회원가입 및 로그인 성공/실패 검증 테스트 코드 리펙토링
    기존에 작성된 UserServiceTest의 회원가입 및 로그인의 성공/실패 검증 테스트 코드를 AssertJ를 사용해서 리펙토링하였다.

--- src/test/java/com/study/sns/domain/user/service/UserServiceTest.java
+++ src/test/java/com/study/sns/domain/user/service/UserServiceTest.java
@@ -1,10 +1,15 @@
 package com.study.sns.domain.user.service;
 
+import com.study.sns.domain.user.account.jwt.JwtService;
+import com.study.sns.domain.user.account.jwt.exception.AccountErrorCode;
+import com.study.sns.domain.user.dto.LocalLoginServiceDto;
 import com.study.sns.domain.user.dto.UserDto;
 import com.study.sns.domain.user.dto.UserJoinServiceDto;
 import com.study.sns.domain.user.model.constant.UserRole;
 import com.study.sns.domain.user.model.constant.UserStatus;
+import com.study.sns.domain.user.model.entity.User;
 import com.study.sns.domain.user.repository.UserRepository;
+import com.study.sns.global.exception.SnsApplicationException;
 import com.study.sns.global.util.fixture.UserFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +21,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
@@ -25,11 +31,13 @@ class UserServiceTest {
     @Autowired
     private UserService userService;
 
-    @MockBean
-    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @MockBean
-    private PasswordEncoder passwordEncoder;
+    private UserRepository userRepository;
+    @MockBean
+    private JwtService jwtService;
 
     @Test
     @DisplayName("회원가입 성공 테스트")
@@ -41,10 +49,12 @@ class UserServiceTest {
         );
         given(userRepository.findByEmail(dto.getEmail()))
                 .willReturn(Optional.empty());
-        given(passwordEncoder.encode(dto.getPassword()))
-                .willReturn("encrypt_password");
-        given(userRepository.save(any()))
-                .willReturn(UserFixture.get(dto.getEmail(), dto.getPassword()));
+        given(userRepository.save(any(User.class)))
+                .willReturn(
+                        UserFixture.get(
+                                dto.getEmail(),
+                                passwordEncoder.encode(dto.getPassword()))
+                );
 
         // When
         UserDto userDto = userService.join(dto);
@@ -55,115 +65,121 @@ class UserServiceTest {
         assertThat(userDto.getStatus()).isEqualTo(UserStatus.ACTIVE);
     }
 
-//    @Test
-//    @DisplayName("회원가입 실패 테스트 - 이미 존재하는 email인 경우")
-//    void 회원가입시_email이_존재하는_경우() throws Exception {
-//        // Given
-//        UserJoinDto.Request req = new UserJoinDto.Request(
-//                "tester@email.com",
-//                "testerPw1234!");
-//
-//        // When
-//        when(userRepository.findByEmail(req.getEmail()))
-//                .thenReturn(Optional.of(
-//                        UserFixture.get(
-//                                req.getEmail(),
-//                                req.getPassword()
-//                        )));
-//        when(passwordEncoder.encode(req.getPassword()))
-//                .thenReturn("encrypt_password");
-//        when(userRepository.save(any()))
-//                .thenReturn(Optional.of(
-//                        UserFixture.get(
-//                                req.getEmail(),
-//                                req.getPassword()
-//                        )));
-//
-//        // Then
-//        SnsApplicationException e = Assertions.assertThrows(
-//                SnsApplicationException.class,
-//                () -> userService.join(req));
-//        Assertions.assertEquals(
-//                AccountErrorCode.DUPLICATED_USER_EMAIL,
-//                e.getErrorCode());
-//    }
+    @Test
+    @DisplayName("회원가입 실패 테스트 - 이미 존재하는 email인 경우")
+    void 회원가입시_email이_존재하는_경우() throws Exception {
+        // Given
+        UserJoinServiceDto dto = new UserJoinServiceDto(
+                "tester@email.com",
+                "testerPw1234!"
+        );
+        given(userRepository.findByEmail(dto.getEmail()))
+                .willReturn(Optional.of(
+                        UserFixture.get(
+                                dto.getEmail(),
+                                passwordEncoder.encode(dto.getPassword())
+                        )
+                ));
+        given(userRepository.save(any(User.class)))
+                .willReturn(
+                        UserFixture.get(
+                                dto.getEmail(),
+                                passwordEncoder.encode(dto.getPassword()))
+                );
 
-//    @Test
-//    @DisplayName("로그인 성공 테스트")
-//    void 로그인_성공() throws Exception {
-//        // Given
-//        LocalLoginDto.Request req = new LocalLoginDto.Request(
-//                "tester@email.com",
-//                "testPw1234!"
-//        );
-//        User fixtureUserEntity = UserFixture.get(
-//                req.getEmail(),
-//                req.getPassword()
-//        );
-//
-//        // When
-//        when(userRepository.findByEmail(req.getEmail()))
-//                .thenReturn(Optional.of(fixtureUserEntity));
-//        when(passwordEncoder.matches(
-//                req.getPassword(),
-//                fixtureUserEntity.getPassword()))
-//                .thenReturn(true);
-//
-//        // Then
-//        Assertions.assertDoesNotThrow(
-//                () -> userService.login(req)
-//        );
-//    }
-//
-//    @Test
-//    @DisplayName("로그인 실패 테스트 - 존재하지 않는 email일 경우")
-//    void 로그인시_email로_회원가입한_사용자가_없는_경우() throws Exception {
-//        // Given
-//        LocalLoginDto.Request req = new LocalLoginDto.Request(
-//                "tester@email.com",
-//                "testPw1234!"
-//        );
-//        // When
-//        when(userRepository.findByEmail(req.getEmail()))
-//                .thenReturn(Optional.empty());
-//
-//        // Then
-//        SnsApplicationException e = Assertions.assertThrows(
-//                SnsApplicationException.class,
-//                () -> userService.login(req)
-//        );
-//        Assertions.assertEquals(
-//                AccountErrorCode.USER_NOT_FOUND,
-//                e.getErrorCode()
-//        );
-//    }
-//
-//    @Test
-//    @DisplayName("로그인 실패 테스트 - 일치하지 않는 password일 경우")
-//    void 로그인시_패스워드_틀린_경우() throws Exception {
-//        // Given
-//        LocalLoginDto.Request req = new LocalLoginDto.Request(
-//                "tester@email.com",
-//                "Wrong Password"
-//        );
-//        String correctPassword = "testPw1234!";
-//
-//        // When
-//        User fixture = UserFixture.get(
-//                req.getEmail(),
-//                correctPassword
-//        );
-//        when(userRepository.findByEmail(req.getEmail()))
-//                .thenReturn(Optional.of(fixture));
-//
-//        // Then
-//        SnsApplicationException e = Assertions.assertThrows(
-//                SnsApplicationException.class,
-//                () -> userService.login(req)
-//        );
-//        Assertions.assertEquals(
-//                AccountErrorCode.INVALID_PASSWORD,
-//                e.getErrorCode()
-//        );
-//    }
+        // When
+        SnsApplicationException e = catchThrowableOfType(
+                () -> {
+                    userService.join(dto);
+                },
+                SnsApplicationException.class
+        );
+
+        // Then
+        assertThat(e.getErrorCode()).isEqualTo(AccountErrorCode.DUPLICATED_USER_EMAIL);
+    }
+
+    @Test
+    @DisplayName("로그인 성공 테스트")
+    void 로그인_성공() throws Exception {
+        // Given
+        LocalLoginServiceDto localLoginDto = new LocalLoginServiceDto(
+                "tester@email.com",
+                "testPw1234!"
+        );
+        given(userRepository.findByEmail(localLoginDto.getEmail()))
+                .willReturn(Optional.of(
+                        UserFixture.get(
+                                localLoginDto.getEmail(),
+                                passwordEncoder.encode(localLoginDto.getPassword())))
+                );
+        given(jwtService.generateToken(localLoginDto.getEmail()))
+                .willReturn(
+                        "valid token"
+                );
+
+        // When
+        String loginToken = userService.login(localLoginDto);
+
+        // Then
+        assertThat(loginToken).isEqualTo("valid token");
+    }
+
+    @Test
+    @DisplayName("로그인 실패 테스트 - 존재하지 않는 email일 경우")
+    void 로그인시_email로_회원가입한_사용자가_없는_경우() throws Exception {
+        // Given
+        LocalLoginServiceDto localLoginDto = new LocalLoginServiceDto(
+                "invalidEmail@email.com",
+                "testPw1234!"
+        );
+        given(userRepository.findByEmail(localLoginDto.getEmail()))
+                .willReturn(Optional.empty());
+        given(jwtService.generateToken(localLoginDto.getEmail()))
+                .willReturn(
+                        "valid token"
+                );
+
+        // When
+        SnsApplicationException e = catchThrowableOfType(
+                () -> {
+                    userService.login(localLoginDto);
+                },
+                SnsApplicationException.class
+        );
+
+        // Then
+        assertThat(e.getErrorCode()).isEqualTo(AccountErrorCode.USER_NOT_FOUND);
+    }
+
+    @Test
+    @DisplayName("로그인 실패 테스트 - 일치하지 않는 password일 경우")
+        void 로그인시_패스워드_틀린_경우() throws Exception {
+        LocalLoginServiceDto localLoginDto = new LocalLoginServiceDto(
+                "tester@email.com",
+                "Invalid Password"
+        );
+        String validPassword = "testerPw1234!";
+        given(userRepository.findByEmail(localLoginDto.getEmail()))
+                .willReturn(Optional.of(
+                        UserFixture.get(
+                                localLoginDto.getEmail(),
+                                passwordEncoder.encode(validPassword)))
+                );
+        given(jwtService.generateToken(localLoginDto.getEmail()))
+                .willReturn(
+                        "valid token"
+                );
+
+        // When
+        SnsApplicationException e = catchThrowableOfType(
+                () -> {
+                    userService.login(localLoginDto);
+                },
+                SnsApplicationException.class
+        );
+
+        // Then
+        assertThat(e.getErrorCode()).isEqualTo(AccountErrorCode.INVALID_PASSWORD);
+    }
 }
