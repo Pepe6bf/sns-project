8ad1706b539b428a70a2f2ba503ce5323ab60539
pepe6bf <pepe6bf@gmail.com>
2022-12-09 14:10:48 +0900

    test: #12 Post 수정 관련 테스트 초안 작성

--- src/test/java/com/study/sns/domain/post/service/PostServiceTest.java
+++ src/test/java/com/study/sns/domain/post/service/PostServiceTest.java
@@ -1,19 +1,16 @@
 package com.study.sns.domain.post.service;
 
-import com.study.sns.domain.post.dto.CreatePostRequest;
 import com.study.sns.domain.post.dto.CreatePostServiceDto;
 import com.study.sns.domain.post.dto.PostDto;
-import com.study.sns.domain.post.exception.PostErrorCode;
+import com.study.sns.domain.post.dto.UpdatePostServiceDto;
 import com.study.sns.domain.post.model.entity.Post;
 import com.study.sns.domain.post.repository.PostRepository;
 import com.study.sns.domain.user.account.jwt.exception.AccountErrorCode;
 import com.study.sns.domain.user.model.entity.User;
-import com.study.sns.domain.user.repository.UserRepository;
 import com.study.sns.domain.user.service.UserService;
-import com.study.sns.global.util.fixture.PostFixture;
 import com.study.sns.global.exception.SnsApplicationException;
+import com.study.sns.global.util.fixture.PostFixture;
 import com.study.sns.global.util.fixture.UserFixture;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,11 +19,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class PostServiceTest {
@@ -101,34 +98,51 @@ public class PostServiceTest {
         assertThat(e.getErrorCode()).isEqualTo(AccountErrorCode.USER_NOT_FOUND);
     }
 
-//    @Test
-//    @DisplayName("게시글 수정 성공 테스트")
-//    void 게시글수정_성공() throws Exception {
-//        // Given
-//        String title = "Test Title";
-//        String content = "Test Content";
-//        String email = "tester@email.com";
-//        Long postId = 1L;
-//
-//        Post mockPost = PostFixture.get(
-//                email,
-//                postId
-//        );
-//        User mockUser = mockPost.getUser();
-//
-//        // When
-//        when(userRepository.findByEmail(email))
-//                .thenReturn(Optional.of(mockUser));
-//        when(postRepository.findById(postId))
-//                .thenReturn(Optional.of(mockPost));
-//
-//        // Then
-//        Assertions.assertDoesNotThrow(
-//                () -> postService.updatePost(
-//                        title,
-//                        content,
-//                        postId
-//                )
-//        );
-//    }
+    @Test
+    @DisplayName("게시글 수정 성공 테스트")
+    void 게시글수정_성공() throws Exception {
+        // Given
+        User currentUser = UserFixture.get(
+                "tester@email.com",
+                "testerPw1234!"
+        );
+        Long postId = 1L;
+        UpdatePostServiceDto dto = new UpdatePostServiceDto(
+                "Test Title",
+                "Test Content"
+        );
+        Post savedPost = PostFixture.get(
+                currentUser,
+                dto.getTitle(),
+                dto.getContent()
+        );
+
+        given(userService.getCurrentUser())
+                .willReturn(mock(User.class));
+        given(postRepository.findById(postId))
+                .willReturn(Optional.of(savedPost));
+
+        // Then
+
+    }
+
+    @Test
+    @DisplayName("게시글 수정 실패 테스트 - 존재하지 않는 게시글 수정 시도")
+    void 포스트수정시_포스트가_존재하지않는_경우() throws Exception {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    @DisplayName("게시글 수정 실패 테스트 - 게시글 수정 권한이 없는 경우")
+    void 게시글수정시_권한이_없는_경우() throws Exception {
+        // Given
+
+        // When
+
+        // Then
+    }
 }
